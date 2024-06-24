25ea653cd366dbc68bbbc5deffdb5c4204988d90
Sanzhar Bissenali <sbissena@caltech.edu>
2024-06-24 16:19:59 -0700

    Quartz sync: Jun 24, 2024, 4:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🌱 Sanzhar Bissenali",
+    pageTitle: "🌱 Sanzhar B. ",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
